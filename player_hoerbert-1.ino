1afd63317d5b9304abb99f50dca23dd1569c6a33
Jiri Heitlager <jiri.heitlager@wooga.net>
2019-02-07 10:43:41 +0100

    Non tested version. This is a refactoring from the initial code, that was not in a repo. This is non compiled and a WIP.

---
+++ player_hoerbert-1.ino
@@ -0,0 +1,284 @@
+/***************************************************
+  This is an example for the Adafruit VS1053 Codec Breakout
+
+  Designed specifically to work with the Adafruit VS1053 Codec Breakout
+  ----> https://www.adafruit.com/products/1381
+
+  Adafruit invests time and resources providing this open source code,
+  please support Adafruit and open-source hardware by purchasing
+  products from Adafruit!
+
+  Written by Limor Fried/Ladyada for Adafruit Industries.
+  BSD license, all text above must be included in any redistribution
+ ****************************************************/
+
+// include SPI, MP3 and SD libraries
+#include <SPI.h>
+#include <Adafruit_VS1053.h>
+#include <SD.h>
+
+// These are the pins used for the music maker shield
+#define SHIELD_RESET  -1      // VS1053 reset pin (unused!)
+#define SHIELD_CS     7      // VS1053 chip select pin (output)
+#define SHIELD_DCS    6      // VS1053 Data/command select pin (output)
+
+// These are common pins between breakout and shield
+#define CARDCS 4     // Card chip select pin
+// DREQ should be an Int pin, see http://arduino.cc/en/Reference/attachInterrupt
+#define DREQ 3       // VS1053 Data request, ideally an Interrupt pin
+
+Adafruit_VS1053_FilePlayer musicPlayer =  Adafruit_VS1053_FilePlayer(SHIELD_RESET, SHIELD_CS, SHIELD_DCS, DREQ, CARDCS);
+
+#define VOLUME_PIN A0 // The volume pin which is connected to the potentiometer.
+#define MAX_VOLUME 5 // Volume is the lower the louder.
+#define MIN_VOLUME 100 // Volume is the lower the louder.
+
+#define BYTE_MAX 255 // To check against an unset byte type.
+
+#define PREV_BUTTON_ID 10 // The number of the button for playing the previous.
+#define NEXT_BUTTON_ID 11 // The number of the button for playing the next.
+
+#define READY_FOR_INPUT  1 // No button is pressed and we can receive a button input.
+#define HANDLING_INPUT  2 // For making sure we only handle one button input.
+
+#define SESSION_TEXT_FILE_PATH "init/with.txt"
+
+int currentVolume = MIN_VOLUME;
+byte pinArray[] = {2, 5, 8, 9, 10};
+byte pinArrayLength = 5;
+int songSelectCursor = -1;
+
+void setup() {
+
+  Serial.begin(9600);
+
+  if (! musicPlayer.begin()) { // initialise the music player
+    Serial.println(F("Couldn't find VS1053, do you have the right pins defined?"));
+    while (1);
+  }
+
+  Serial.println(F("VS1053 found"));
+
+  if (!SD.begin(CARDCS)) {
+    Serial.println(F("SD failed, or not present"));
+    while (1);  // don't do anything more
+  }
+
+  for (uint8_t count = 0; count < pinArrayLength; count++) {
+    pinMode(pinArray[count], INPUT_PULLUP);
+  }
+
+  for (uint8_t i = 0; i < 7; i++) {
+    musicPlayer.GPIO_digitalWrite(i, HIGH);
+  }
+
+  BuildPlaylistIndex();
+
+  musicPlayer.useInterrupt(VS1053_FILEPLAYER_PIN_INT);
+  musicPlayer.sineTest(0x44, 600);    // Make a tone to indicate all is fine and initilized
+}
+
+// I could consider storing this data in a file on the SD, so we dont need to build the index at startup.
+// It would be faster to load the file and parse it. It is also feasible because we create the data on the SD ourselves.
+int sumFilesPerFolderCache[10] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+void BuildPlaylistIndex() {
+
+  for (int i = 0; i < 10; i++) {
+    String path =  "audio/" + char(i);
+
+    File audioDirectory = SD.open(path, O_READ);
+    audioDirectory.rewindDirectory();
+    int sumFiles = 0;
+    while ( true )
+    {
+      File entry =  audioDirectory.openNextFile();
+      if ( entry )
+      {
+        String file_name = entry.name();
+        entry.close();
+
+        const char tilde = '~';
+
+        if ( file_name.indexOf(tilde) == -1 )
+        {
+          ++sumFiles;
+        }
+      } else {
+        audioDirectory.rewindDirectory();
+        audioDirectory.close();
+        break;
+      }
+    }
+    sumFilesPerFolderCache[i] = sumFiles;
+  }
+}
+
+byte inputstate = READY_FOR_INPUT;
+byte currentDirIndex = BYTE_MAX;
+void loop() {
+  byte buttonPressed = GetCurrentPressedButton();
+
+  if (inputstate == HANDLING_INPUT && buttonPressed == BYTE_MAX) {
+    inputstate = READY_FOR_INPUT;
+  } else if (inputstate == READY_FOR_INPUT && buttonPressed != BYTE_MAX) {
+    inputstate = HANDLING_INPUT;
+    OnButtonPressed(buttonPressed);
+  }
+  UpdateVolume();
+}
+
+byte GetCurrentPressedButton() {
+  for (byte i = 0; i < 7; ++i) {
+    byte reading = musicPlayer.GPIO_digitalRead(i + 1);
+    bool pressedDown = reading == HIGH;
+    if (pressedDown) {
+      return i;
+    }
+  }
+
+  for (byte i = 0; i < pinArrayLength; ++i) {
+    byte pinNumber = pinArray[i];
+    bool pressedDown = digitalRead(pinNumber) == LOW;
+    if (pressedDown) {
+      return i + 7;
+    }
+  }
+  return BYTE_MAX;
+}
+
+void OnButtonPressed(byte buttonPressedId) {
+
+  int next = 0;
+
+  switch (buttonPressedId) {
+    case BYTE_MAX:
+      // illegal button id.
+      return;
+    case NEXT_BUTTON_ID:
+      if(currentDirIndex != BYTE_MAX){
+        Serial.print("Play next from dir: ");
+        Serial.print(currentDirIndex);
+        Serial.println();
+        next = 1;
+      }
+      break;
+    case PREV_BUTTON_ID:
+      if(currentDirIndex != BYTE_MAX){
+        Serial.print("Play previous from dir: ");
+        Serial.print(currentDirIndex);
+        Serial.println();
+        next = -1;
+      }
+      break;
+    default:
+      bool shouldReset = currentDirIndex != buttonPressedId;
+      if(shouldReset){
+        Reset(buttonPressedId, -1);
+      }
+      next = 1;
+  }
+
+  int nextFileIndex = GetNextFileIndex(currentDirIndex, next);
+  Play(currentDirIndex, nextFileIndex);
+}
+
+bool IsControlButton(byte buttonId){
+  return buttonId == NEXT_BUTTON_ID || buttonId == PREV_BUTTON_ID
+}
+
+void Reset(int dirIndex, int songIndex){
+  Serial.print("Resetting dir from ");
+  Serial.print(currentDirIndex);
+  Serial.print(" to " );
+  Serial.print(dirIndex);
+  Serial.println();
+
+  Serial.print("Resetting songIndex from ");
+  Serial.print(songSelectCursor);
+  Serial.print(" to " );
+  Serial.println(songIndex);
+  Serial.println();
+
+  currentDirIndex = dirIndex;
+  songSelectCursor = songIndex;
+}
+
+int GetNextFileIndex(int dirIndex, int direction){
+  int next = songSelectCursor + dir;
+  int numberOfFiles = sumFilesPerFolderCache[dirIndex];
+  if (next <= 0 || next >= numberOfFiles)
+  {
+    return songSelectCursor;
+  }
+  songSelectCursor = next;
+  return songSelectCursor;
+}
+
+void ContinuePlayingFromSession(){
+  // if(SD.exists(SESSION_TEXT_FILE_PATH)){
+  //     File textFile = SD.open(SESSION_TEXT_FILE_PATH, O_READ);
+  //     string finalString="";
+  //     while (textFile.available()){
+  //       finalString+=(char)textFile.read();
+  //     }
+  // }
+  int directoryIndexInFile = 3;
+  int songIndexInFile = 1;
+  Reset(directoryIndexInFile, songIndexInFile);
+  Play(currentDirIndex,songSelectCursor);
+}
+
+void Play(int dirIndex, int songIndex){
+
+  Serial.print("Request to start playing file : ");
+  Serial.print(dirIndex);
+  Serial.print(" / " );
+  Serial.print(songIndex);
+  Serial.println();
+
+  String songSelectorAsString = String(songIndex);
+  String dirIndexAsString = String(dirIndex);
+  String audioFile = "audio/" + dirIndexAsString + "/t_" + dirIndexAsString + "_" + songSelectorAsString + ".mp3";
+
+  // Length (with one extra character for the null terminator)
+  int str_len = audioFile.length() + 1;
+  //  // Prepare the character array (the buffer)
+  char char_array[str_len];
+
+  // Copy it over
+  audioFile.toCharArray(char_array, str_len);
+
+  // pausing it first prevents audible glitchy sounds.
+  musicPlayer.pausePlaying(true);
+  // now stop it.
+  musicPlayer.stopPlaying();
+  // we can only write to the SD card when the audio is stopped.
+  // SD is either read or write.
+  PersistCurrentSelectedData(dirIndexAsString, songSelectorAsString);
+
+  musicPlayer.startPlayingFile(char_array);
+}
+
+void PersistCurrentSelectedData(String dirIndexAsString, String songSelectorAsString) {
+  File sessionTextFile = SD.open(SESSION_TEXT_FILE_PATH, O_CREAT | O_TRUNC | O_WRITE);
+  sessionTextFile.println(dirIndexAsString + "," + songSelectorAsString);
+  sessionTextFile.close();
+}
+
+void UpdateVolume() {
+  // read the state of the volume potentiometer
+  int pinValue = analogRead(VOLUME_PIN);
+
+  // set the range of the volume from 0 to 100
+  pinValue = map(pinValue, 0, 1023, MIN_VOLUME, MAX_VOLUME);
+
+  // recognize state (volume) changes in steps of two
+  int stepSize = 1;
+  if (pinValue < volumeState - stepSize || pinValue > volumeState + stepSize)
+  {
+    // remember the new volume state
+    currentVolume = pinValue;
+    musicPlayer.setVolume(currentVolume, currentVolume);
+  }
+  delay(2); // delay in between reads for stability
+}
