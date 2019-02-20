c771a7cff21c8ba2bc3f765089ad7a6dc427a834
Jiri Heitlager <jiri.heitlager@wooga.net>
2019-02-20 11:36:51 +0100

    Code clean. Saves a nfo.txt file that contains the max number of file entries per audio folder. This way we dont need to read the whole folder structure on the SD card to find out the number of files per folder. This should save startup time.

--- ruby/AudioBox.rb
+++ ruby/AudioBox.rb
@@ -6,35 +6,51 @@ require 'json'
 puts "Runing conversion script"
 
 glob = Dir.glob('data/*')
-sumFolder = glob.length
 date = DateTime.now.strftime('%s')
 manifest = {}
-folderCount = 0
+files_count_array = Array.new
 outputFolder = "output-#{date}"
+folder_counter = 0
+files_counter = 0
 total_size = 0
 
+# we only have 10 buttons on the audiobox so more folders is not allowed.
 if glob.length <= 10
- glob.sort.each do |folder|
-   # do something with the file here
-   if(File.directory?(folder))
-     foldername = File.basename(folder)
-     manifest[foldername] = {}
-     # puts foldername
-     path = "#{outputFolder}/#{folderCount}"
-     folderCount +=1
-     FileUtils.mkdir_p path
-     i = 0
-     Dir.glob("#{folder}/**/*.mp3").sort.each do |filename|
-       basename = File.basename(filename)
-       outName = "#{i}.mp3"
-       manifest[foldername][filename] = "#{path}/#{outName}"
-        FileUtils.cp(filename, "#{path}/#{outName}")
-        total_size+=File.size(filename)
-       i+=1
-     end
 
-   end
- end
+  # go into each audio folder
+  glob.sort.each do |file_in_audio_folder|
+
+    unless File.file?(file_in_audio_folder)
+      file_parent_folder_basename = File.basename(file_in_audio_folder)
+      manifest[folder_counter] = {}
+      path = "#{outputFolder}/#{folder_counter}"
+
+      FileUtils.mkdir_p path
+
+      files = Dir.glob("#{file_in_audio_folder}/**/*.mp3").sort
+      files_count_array[folder_counter] = files.length
+
+      files.each do |filename|
+      basename = File.basename(filename)
+      outName = "#{files_counter}.mp3"
+      manifest[folder_counter][filename] = "#{path}/#{outName}"
+      FileUtils.cp(filename, "#{path}/#{outName}")
+      total_size+=File.size(filename)
+      files_counter +=1
+    end
+
+    files_counter = 0
+    folder_counter +=1
+    end
+  end
+end
+
+File.open("#{outputFolder}/nfo.txt","w") do |f|
+  f.write(files_count_array.join(","))
+end
+
+File.open("#{outputFolder}/manifest.json","w") do |f|
+  f.write(manifest.to_json)
 end
 
 MEGABYTE = 1024.0 * 1024.0
@@ -44,7 +60,4 @@ end
 
 # puts total_size.to_s + ' bytes'  # displays 62651176 bytes
 puts bytesToMeg(total_size).to_s + ' MB'  # displays 59.7488174438477 MB
-
-File.open("#{outputFolder}/manifest.json","w") do |f|
-  f.write(manifest.to_json)
-end
+puts JSON.pretty_generate(manifest)
